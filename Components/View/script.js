96530b227ce4a2fd83a32851d41793521ba66808
Name <9023993@student.zadkine.nl>
2025-03-07 12:52:23 +0100

    12:52 7-3-2025 update

--- Components/View/script.js
+++ Components/View/script.js
@@ -8,22 +8,33 @@ const cards = [
         cost: 4,
         attack: 6,
         health: 5,
-            image: "../Components/View/images/dragon.jpg"
+        image: "dragon.jpg",
+        rarity: "Legendary"
     },
     {
         name: "Vuurballen",
         type: "spreuk",
         cost: 3,
         damage: 5,
-        image: "images/"
+        image: "2.jpg",
+        rarity: "Rare"
     },
     {
-        name: "Mana Crystal",
+        name: "Green Elixer",
         type: "artefact",
         cost: 2,
         effect: "Verdubbelt de mana per beurt.",
-        image: "images/"
-    }
+        image: "3.jpg",
+        rarity: "Common"
+    },
+    {
+        name: "Hog Rider",
+        type: "wezen",
+        cost: 2,
+        attack: 3,
+        image: "1.jpg",
+        rarity: "Epic"
+    },
 ];
 
 function drawCards() {
@@ -36,23 +47,23 @@ function displayHand() {
     handDiv.innerHTML = "";
     playerHand.forEach((card, index) => {
         const cardDiv = document.createElement("div");
-        cardDiv.className = "card";
-
+        cardDiv.className = `card ${card.rarity.toLowerCase()}`;
 
         const img = document.createElement("img");
         img.src = card.image;
         img.alt = card.name;
-        img.style.width = "80%";
+        img.style.width = "100%";
         img.style.height = "auto";
 
-
         const textDiv = document.createElement("div");
-        textDiv.innerHTML = `<p>${card.name}</p><p>Kosten: ${card.cost}</p>`;
-
+        textDiv.innerHTML = `
+            <p class="${card.rarity.toLowerCase()}">${card.name}</p>
+            <p class="${card.rarity.toLowerCase()}">${card.rarity}</p>
+            <p class="${card.rarity.toLowerCase()}">${card.cost}</p>
+        `;
 
         cardDiv.appendChild(img);
         cardDiv.appendChild(textDiv);
-
         cardDiv.onclick = () => playCard(index);
         handDiv.appendChild(cardDiv);
     });
@@ -60,12 +71,16 @@ function displayHand() {
 
 function playCard(index) {
     const card = playerHand[index];
-    if (mana >= card.cost) {
+    if (card.name === "Green Elixer") {
+        mana += 2;
+        document.getElementById("mana-count").innerText = mana;
+        alert(card.name + " gebruikt! Je krijgt 2 mana.");
+        playerHand.splice(index, 1);
+        displayHand();
+    } else if (mana >= card.cost) {
         mana -= card.cost;
         document.getElementById("mana-count").innerText = mana;
         alert(card.name + " gespeeld!");
-
-
         playerHand.splice(index, 1);
         displayHand();
     } else {
